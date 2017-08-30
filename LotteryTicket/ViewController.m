81521f7bd5c03c2628f53af3be4326846b0c6e09
Dobby <jevo747@yeah.net>
2017-08-30 10:25:18 +0800

    Initial Commit

---
+++ LotteryTicket/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LotteryTicket
+//
+//  Created by v_ljiayili(李嘉艺) on 2017/8/30.
+//  Copyright © 2017年 Dobby. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ LotteryTicket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LotteryTicket
+//
+//  Created by v_ljiayili(李嘉艺) on 2017/8/30.
+//  Copyright © 2017年 Dobby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
